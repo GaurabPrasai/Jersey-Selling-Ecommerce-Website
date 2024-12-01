e4e50686cc57b251cf16e9572f7d4dd1a49cf298
Gaurab Prasai <78253021+GaurabPrasai@users.noreply.github.com>
2024-12-01 15:56:40 +0545

    bugfix: bg-img was not rendering

--- E-commerce/src/App.jsx
+++ E-commerce/src/App.jsx
@@ -1,4 +1,24 @@
-
+const Head = () => {
+  return(
+    <>
+      <head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Gaurab's Jersey Store</title>
+      <link rel="stylesheet" href="style.css" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100..900;1,100..900&display=swap"
+          rel="stylesheet" />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+          integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+          crossorigin="anonymous" referrerpolicy="no-referrer" />
+      <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css" />
+      </head>
+    </>
+  )
+}
 const Logo = () => {
   return(
     <>
@@ -187,7 +207,7 @@ const ContactSection = () => {
       <section className="contact" id="cont">
         <div className="contact-info">
             <div className="first-info">
-                <img src="img/logo.png" alt="" />
+                <img src="../src/assets/img/logo.png" alt="" />
                 <p>Sundarharaincha-10 Khorsane <br /> Morang, Nepal</p>
                 <p>021-5606032</p>
                 <p>GaurabPrasaigp@gmail.com</p>
@@ -229,6 +249,7 @@ const ContactSection = () => {
     </>
   );
 }
+
 const Section = () => {
   return(
     <>
@@ -255,6 +276,7 @@ const AppLayout = () => {
 
   return (
     <>
+      <Head />
       <Header />
       <Section />
       <Fotter />
